c92be91bc9ede6a5dc849c704f2852e9450bb25d
tony-cannibal <castrovital.luis@gmail.com>
2022-10-18 17:23:09 -0500

    Removed Scroll Changes Workspace

--- rc.lua
+++ rc.lua
@@ -255,9 +255,9 @@ awful.screen.connect_for_each_screen(function(s) beautiful.at_screen_connect(s) 
 -- {{{ Mouse bindings
 
 root.buttons(mytable.join(
-    awful.button({ }, 3, function () awful.util.mymainmenu:toggle() end),
-    awful.button({ }, 4, awful.tag.viewnext),
-    awful.button({ }, 5, awful.tag.viewprev)
+    awful.button({ }, 3, function () awful.util.mymainmenu:toggle() end)
+    -- awful.button({ }, 4, awful.tag.viewnext),
+    -- awful.button({ }, 5, awful.tag.viewprev)
 ))
 
 -- }}}
